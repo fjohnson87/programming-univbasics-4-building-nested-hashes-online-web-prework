12652548df4fdf662e8b392aefd4afd55a239c4c
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-05 17:35:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,9 +27,10 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  :railroads [:names] ={:pennsylvania_railroads =>{:reading_railroad
-  :pennsylvania_railroads
-  :b_and_o_railroads
-  :shortline_railroad}}
+  :railroads [:names] ={:pennsylvania_railroads =>
+  {:reading_railroad =>
+  :pennsylvania_railroads=>
+  :b_and_o_railroads=>
+  :shortline_railroad => }}
   
 end
