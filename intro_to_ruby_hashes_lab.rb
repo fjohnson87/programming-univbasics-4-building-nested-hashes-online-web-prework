369649df45bf3001f0a1f0d6b007c1b50857bfdc
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-05 17:28:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,4 +27,8 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  :reading_railroad
+  :pennsylvania_railroads
+  :b_and_o_railroads
+  :shortline
 end
